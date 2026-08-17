b1258752e41b63ee9abea9e56ade608bee4c5b5d
Sternenfisch <105643443+RobertBoettcherSF@users.noreply.github.com>
2026-08-17 14:12:01 +0200

    Implement main procedure with startup messages

---
+++ main.adb
@@ -0,0 +1,7 @@
+with Ada.Text_IO; use Ada.Text_IO;
+
+procedure Main is
+begin
+   Put_Line ("Generalised Hough Transform implementation successfully compiled.");
+   Put_Line ("Run 'make test' or execute 'bin/tests' to run the test suite.");
+end Main;
